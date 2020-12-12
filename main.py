0abbdf2bb5bdd9e498656ec362b6cc6d8789ff20
Kris Barber <kristopher.p.barber@gmail.com>
2020-12-12 17:09:42 -0500

    basic range handling

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
             posts_url = var
         else:
             var = None
-
+    '''
     
     var = None
     while (var == None):
@@ -32,6 +32,11 @@ def main():
         matched = re.match(r"^\d\d\d\d/\d\d$", var)
         if (matched):
             start_date = var
+            start_year = int(start_date[0:4])
+            start_month = int(start_date[5:])
+            if (start_month > 12 or start_month < 1):
+                print("months must be between 1 and 12")
+                var = None
         else:
             var = None
         
@@ -41,6 +46,17 @@ def main():
         matched = re.match(r"^\d\d\d\d/\d\d$", var)
         if (matched):
             end_date = var
+            end_year = int(end_date[0:4])
+            end_month = int(end_date[5:])
+            if (end_year < start_year):
+                print("end year must be after start year")
+                var = None
+            if (end_year == start_year) & (end_month < start_month):
+                print("end month must be after start month")
+                var = None
+            if (end_month > 12 or end_month < 1):
+                print("months must be between 1 and 12")
+                var = None
         else:
             var = None
     
@@ -48,38 +64,41 @@ def main():
         print("Url: " + str(posts_url))
         print("Start Date: " + str(start_date))
         print("End Date: " + str(end_date))
+
+    for year in range(start_year, end_year+1):
+        if (year == start_year):
+            loop_start_month = start_month
+        else :
+            loop_start_month = 1
+        if (year == end_year):
+            loop_end_month = end_month + 1
+        else :
+            loop_end_month = 12
+        
+        for month in range(loop_start_month, loop_end_month):
+            if (debug): print(str(year) + "-" + str(month))
+            query_url = posts_url + "?filters[month]=" + str(year) + "-" + str(month)
+            if (debug): print(query_url)
+            inversion_url = query_url + "&sort=published_at"
+            if (debug): print(inversion_url)
+
+    print ("Done")
+
+
     '''
-
-
-    #firefox_capabilities = DesiredCapabilities.FIREFOX
-    #firefox_capabilities['marionette'] = True
-
-    options = Options()
-    options.add_argument("--headless")
-    #options.add_argument("-profile")
-    #options.add_argument("C:/Users/redyo/AppData/Local/Mozilla/Firefox/Profiles/rf9xcvji.Selenium")
-    #options.add_argument("C:/Users/redyo/AppData/Roaming/Mozilla/Firefox/Profiles/rf9xcvji.Selenium")
-    #options.add_argument("--marionette")
-    #driver = webdriver.Firefox(options=options, capabilities=firefox_capabilities)
-
-    '''
-    cwd = os.getcwd()
-    path = cwd + "\geckodriver.exe"
-    print(path)
-    binary = FirefoxBinary(firefox_path=path, log_file=cwd + "\log.txt")
-    
     driver = webdriver.Firefox()
     driver.get(posts_url)
     var = None
     while (var != "Y"):
         var = input("Done logging in? (Y/N)")
-    f = open("source_1.txt", "w", encoding='utf-8')
-    f.write(driver.page_source)
-    f.close()
+
+
     pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
     driver.close()
     time.sleep(5)
-    '''
+    
+    options = Options()
+    options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     driver.get(posts_url)
     for cookie in pickle.load(open("cookies.pkl", "rb")):
@@ -89,5 +108,6 @@ def main():
     f = open("source_2.txt", "w", encoding='utf-8')
     f.write(driver.page_source)
     f.close()
+    '''
 
 main()
