6430b2351c42996351f6f2b29600791f66c400f1
Kris Barber <kristopher.p.barber@gmail.com>
2020-12-14 14:34:43 -0500

    basic scraping

--- main.py
+++ main.py
@@ -6,10 +6,63 @@ import re
 import os
 import pickle 
 
-
+driver = None
 debug = False
+links = []
+
+def scrape_month(url):
+    global driver
+    global links
+    if (driver is None):
+        return
+
+    driver.get(query_url)
+    height = driver.execute_script("return document.body.scrollHeight")
+    x = 0
+    stop = False
+
+    while (stop != True):
+        while x < height:
+            time.sleep(0.01)
+            driver.execute_script("window.scrollTo(0, " + str(x) +");")
+            x = x + 100
+        time.sleep(1)
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if (new_height != height):
+            height = new_height
+            stop = False
+            continue
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        titles = soup.find_all(attrs={'data-tag':'post-title'})
+        unique_post_found = False
+        for link in titles:
+            a = link.find("a")
+            if (a):
+                href = a.get('href')
+                if (href not in links & re.match(r"patreon.com/posts/", href)):
+                    unique_post_found = True
+                    links.append(href)
+
+        if (unique_post_found):
+            a_string = soup.find(string=re.compile(r"^Load more$"))
+            button = a_string.find_parent("button")
+            if(button):
+                class_list = button['class']
+                class_string = ".".join(class_list)
+                more_button = driver.find_element_by_class_name(class_string)
+                more_button.click()
+            else:
+                stop = True
+        else:
+            stop = True
+    
+
 
 def main():
+    global driver
     print("Hello World")
 
     posts_url = "https://www.patreon.com/AldhaRoku/posts"
@@ -18,7 +71,7 @@ def main():
     start_month = 1
     end_month = 1
 
-    '''
+    
     var = None
     while (var == None):
         var = input("Enter a patreon posts tab URL (patreon.com/artist/posts):")
@@ -72,19 +125,24 @@ def main():
         print("Start Date: " + str(start_date))
         print("End Date: " + str(end_date))
 
-    '''
     
     driver = webdriver.Firefox()
     driver.get(posts_url)
-    if (os.path.isfile("cookies.pkl")):
-        for cookie in pickle.load(open("cookies.pkl", "rb")):
+
+    if (os.path.isfile("patreon_cookie.pkl")):
+        for cookie in pickle.load(open("patreon_cookie.pkl", "rb")):
             driver.add_cookie(cookie)
+    
+    '''
         driver.get(posts_url)
-    else:
-        var = None
-        while (var != "Y"):
-            var = input("Done logging in? (Y/N)")
-        pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+
+    var = None
+    while (var != "Y"):
+        var = input("Done logging in? (Y/N)")
+    '''
+    pickle.dump(driver.get_cookies(), open("patreon_cookie.pkl", "wb"))
+    
+    links = []
 
     for year in range(start_year, end_year+1):
         if (year == start_year):
@@ -105,58 +163,22 @@ def main():
 
             # Go down one way
 
-            driver.get(query_url)
+            scrape_month(query_url)
 
             time.sleep(1)
 
-            height = driver.execute_script("return document.body.scrollHeight")
-            x = 0
-            stop = False
+            # Go down the other way
 
-            while (stop != True):
-                while x < height:
-                    time.sleep(0.05)
-                    driver.execute_script("window.scrollTo(0, " + str(x) +");")
-                    x = x + 100
-                time.sleep(5)
-
-                new_height = driver.execute_script("return document.body.scrollHeight")
-                if (new_height != height):
-                    stop = False
-                    continue
-
-                print(soup.find(string=re.compile("Load more")))
-
-            return
-
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            titles = soup.find_all(attrs={'data-tag':'post-title'})
-            links = []
-            for link in titles:
-                a = link.find("a")
-                links.append(a.get('href'))
+            scrape_month(inversion_url)
 
             print(links)
 
+    with open('links.txt', mode="wt", encoding="utf-8") as myfile:
+        myfile.write('\n'.join(links))
             
+    #with open('links.txt') as f:
+    #    links = f.read().splitlines()
 
 
-    '''
-    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
-    driver.close()
-    time.sleep(5)
-    
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
-    driver.get(posts_url)
-    for cookie in pickle.load(open("cookies.pkl", "rb")):
-        driver.add_cookie(cookie)
-    driver.get(posts_url)
-    time.sleep(5)
-    f = open("source_2.txt", "w", encoding='utf-8')
-    f.write(driver.page_source)
-    f.close()
-    '''
 
 main()
