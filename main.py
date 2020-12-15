0a25c09c8dc269d36b624e0cd49935f65282dd1c
Kris Barber <kristopher.p.barber@gmail.com>
2020-12-14 19:41:19 -0500

    Downloading

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import ActionChains
+from selenium.common import exceptions
 from bs4 import BeautifulSoup
 import time
 import re
 import os
+import pathlib
 import pickle 
 
 driver = None
-debug = False
-links = []
+debug = True
+post_links = []
 
-def scrape_month(url):
+def scrape_month(query_url):
     global driver
-    global links
+    global post_links
     if (driver is None):
         return
 
@@ -24,53 +27,73 @@ def scrape_month(url):
     while (stop != True):
         while x < height:
             time.sleep(0.01)
+            if debug: print("Scroll to: " + str(x))
             driver.execute_script("window.scrollTo(0, " + str(x) +");")
             x = x + 100
-        time.sleep(1)
+        time.sleep(3)
 
         new_height = driver.execute_script("return document.body.scrollHeight")
         if (new_height != height):
+            if debug: print("Page extended")
             height = new_height
             stop = False
             continue
 
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+        loader = soup.find(attrs={"aria-label":"loading more posts"})
+        if (loader): continue
+
         titles = soup.find_all(attrs={'data-tag':'post-title'})
+        if debug: print("Titles found: ")
+        if debug: print(titles)
         unique_post_found = False
         for link in titles:
             a = link.find("a")
+            if debug: print("a: " + str(a))
             if (a):
                 href = a.get('href')
-                if (href not in links & re.match(r"patreon.com/posts/", href)):
-                    unique_post_found = True
-                    links.append(href)
+                if debug: print("href: " + href)
+                if debug: print(re.match(r"^https://www\.patreon\.com/posts/", href))
+                if (re.match(r"^https://www\.patreon\.com/posts/", href)):
+                    if debug: print("regex match")
+                    if (href not in post_links):
+                        if debug: print("Unique Link: " + href)
+                        unique_post_found = True
+                        post_links.append(href)
 
         if (unique_post_found):
             a_string = soup.find(string=re.compile(r"^Load more$"))
+            if debug: print(a_string)
             button = a_string.find_parent("button")
+            if debug: print(button)
             if(button):
                 class_list = button['class']
                 class_string = ".".join(class_list)
+                if debug: print(class_string)
                 more_button = driver.find_element_by_class_name(class_string)
+                if debug: print(more_button)
                 more_button.click()
             else:
                 stop = True
         else:
             stop = True
     
+    if debug: print(post_links)
+    
 
 
 def main():
     global driver
-    print("Hello World")
+    global post_links
 
+    '''
     posts_url = "https://www.patreon.com/AldhaRoku/posts"
     start_year = 2020
     end_year = 2020
-    start_month = 1
-    end_month = 1
-
+    start_month = 11
+    end_month = 11
+    '''
     
     var = None
     while (var == None):
@@ -124,7 +147,6 @@ def main():
         print("Url: " + str(posts_url))
         print("Start Date: " + str(start_date))
         print("End Date: " + str(end_date))
-
     
     driver = webdriver.Firefox()
     driver.get(posts_url)
@@ -132,18 +154,16 @@ def main():
     if (os.path.isfile("patreon_cookie.pkl")):
         for cookie in pickle.load(open("patreon_cookie.pkl", "rb")):
             driver.add_cookie(cookie)
-    
-    '''
         driver.get(posts_url)
 
     var = None
     while (var != "Y"):
         var = input("Done logging in? (Y/N)")
-    '''
+
     pickle.dump(driver.get_cookies(), open("patreon_cookie.pkl", "wb"))
     
-    links = []
-
+    post_links = []
+    
     for year in range(start_year, end_year+1):
         if (year == start_year):
             loop_start_month = start_month
@@ -165,20 +185,81 @@ def main():
 
             scrape_month(query_url)
 
+            if debug: print(post_links)
+
             time.sleep(1)
 
             # Go down the other way
 
             scrape_month(inversion_url)
 
-            print(links)
+            if debug: print(post_links)
 
-    with open('links.txt', mode="wt", encoding="utf-8") as myfile:
-        myfile.write('\n'.join(links))
+    with open('post_links.txt', mode="wt", encoding="utf-8") as myfile:
+        myfile.write('\n'.join(post_links))
+    
+    with open('post_links.txt') as f:
+        post_links = f.read().splitlines()
+
+    links = []
+
+    pathlib.Path(os.getcwd()+ "/scraped/").mkdir(parents=True, exist_ok=True)
+
+    for post_link in post_links:
+        driver.get(post_link)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        title = soup.find(attrs={"data-tag":"post-title"})
+        title = title.contents[0]
+        title = "".join([c for c in title if c.isalpha() or c.isdigit()]).rstrip()
+
+        content = soup.find_all(attrs={"data-tag":"post-content"})
+        if (content):
+            for content_piece in content:
+                content_links = content_piece.find_all("a")
+                if (content_links):
+                    for link in content_links:
+                        href = link.get("href")
+                        if (href not in links):
+                            links.append(href)
+
+        images = driver.find_elements_by_xpath('//img[@data-pin-nopin="true"]')
+        driver.scopes = [
+            '.*c10\.patreonusercontent.*'
+        ]
+        for image in images:
+            del driver.requests
+            image.click()
+            driver.wait_for_request("c10.patreonusercontent")
+            request = driver.last_request
+            image_url = request.url
+            if (request.response):
+                filename = image_url[image_url.rindex("/") + 1:image_url.rindex(".")]
+                fileextension = image_url[image_url.rindex("."):image_url.rindex("?")]
+                pathlib.Path(os.getcwd()+ "/scraped/" + title + "/").mkdir(parents=True, exist_ok=True)
+                path = os.getcwd()+ "/scraped/" + title + "/" + filename + fileextension
+                if (os.path.isfile(path)):
+                    repeat = 1
+                    while (os.path.isfile(path)):
+                        path = os.getcwd()+ "/scraped/" + title + "/" + filename + " (" + str(repeat) + ")" + fileextension
+                        repeat = repeat + 1
             
-    #with open('links.txt') as f:
-    #    links = f.read().splitlines()
+                output = open(path, "wb")
+                output.write(request.response.body)
+                output.close()
+            time.sleep(1)
+            box = driver.find_element_by_xpath('//div[@data-target="lightbox-content"]')
+            while(box):
+                driver.execute_script("arguments[0].click()", box)
+                try:
+                    box = driver.find_element_by_xpath('//div[@data-target="lightbox-content"]')
+                except exceptions.NoSuchElementException as err:
+                    box = None
+                    pass   
+                time.sleep(1)
 
+    with open(os.getcwd() + '/scraped/links.txt', mode="wt", encoding="utf-8") as myfile:
+        myfile.write('\n'.join(links))
 
 
 main()
